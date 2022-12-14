a8bb1027e308f494a7720fbf7d9fd4d446307186
Irchkhitunidze <irchkhitunidze@tbcbank.com.ge>
2022-12-14 23:27:24 +0400

    project init

---
+++ FructApp_SwiftUI/FructApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructApp_SwiftUIApp.swift
+//  FructApp_SwiftUI
+//
+//  Created by IKAKOOO on 12/14/22.
+//
+
+import SwiftUI
+
+@main
+struct FructApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
