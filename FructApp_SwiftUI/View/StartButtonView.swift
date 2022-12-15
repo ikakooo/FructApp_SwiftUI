0b1d6796a9d355dddf8f9070a172a2ac58c0259d
Irakli Chkhitunidze <43764577+ikakooo@users.noreply.github.com>
2022-12-16 01:55:14 +0400

    navigation on button click

--- FructApp_SwiftUI/View/StartButtonView.swift
+++ FructApp_SwiftUI/View/StartButtonView.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 struct StartButtonView: View {
     // MARK: PROPERTIES
+    @AppStorage("isOnboarding") var isOnboarding: Bool?
     
     // MARK: BODY
     var body: some View {
         Button(action: {
+            isOnboarding = false
             print("Exit the onboarding")
         }){
             HStack {
